2e2b55ac1e3ed3679fcf6f6bc6847c2611078f8e
Marsot Pierre <pierre.mmarsot@gmail.com>
2022-05-30 15:40:07 +0200

    update

--- generateMerkle.js
+++ generateMerkle.js
@@ -4,18 +4,23 @@ const keccak256 = require("keccak256");
 const fs = require('fs');
 
 function main() {
-  const data = {
-    "0x73968b9a57c6e53d41345fd57a6e6ae27d6cdb2f": [
-      { index: 0, address: "0xD08c8e6d78a1f64B1796d6DC3137B19665cb6F1F", amount: 10 },
-      { index: 0, address: "0xb7D15753D3F76e7C892B63db6b4729f700C01298", amount: 15 },
-      { index: 0, address: "0xf69Ca530Cd4849e3d1329FBEC06787a96a3f9A68", amount: 30 },
-    ]
-  };
+  const data = [
+    {
+      "symbol": "SDT",
+      "address": "0x73968b9a57c6e53d41345fd57a6e6ae27d6cdb2f",
+      "users": [
+        { index: 0, address: "0xD08c8e6d78a1f64B1796d6DC3137B19665cb6F1F", amount: 10 },
+        { index: 0, address: "0xb7D15753D3F76e7C892B63db6b4729f700C01298", amount: 15 },
+        { index: 0, address: "0xf69Ca530Cd4849e3d1329FBEC06787a96a3f9A68", amount: 30 },
+      ]
+    }
+  ];
 
-  const global = {};
+  const global = [];
 
   for (const key of Object.keys(data)) {
-    const users = data[key];
+    const elem = data[key];
+    const users = elem.users;
 
     const elements = users.map((x) =>
       utils.solidityKeccak256(["uint256", "address", "uint256"], [x.index, x.address, x.amount])
@@ -23,20 +28,22 @@ function main() {
 
     const merkleTree = new MerkleTree(elements, keccak256, { sort: true });
 
-    let res = {
-      "claims": {}
-    };
+    let res = {};
 
     for (let i = 0; i < users.length; i++) {
       const user = users[i];
-      res["claims"][user.address] = {
+      res[user.address] = {
         index: user.index,
         amount: user.amount,
         proof: merkleTree.getHexProof(elements[i])
       };
     }
 
-    global[key] = res;
+    global.push({
+      "symbol": elem.symbol,
+      "address": elem.address,
+      "merkle": res
+    });
   }
 
   fs.writeFileSync('merkle.json', JSON.stringify(global));
