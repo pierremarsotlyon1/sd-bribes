ce11747ab4a0c2ed0118741dd38e77072ce8bf94
Marsot Pierre <pierre.mmarsot@gmail.com>
2022-05-31 12:32:50 +0200

    new test

--- generateMerkle.js
+++ generateMerkle.js
@@ -1,16 +1,19 @@
 const { MerkleTree } = require("merkletreejs");
-const { utils } = require("ethers");
+const { utils, BigNumber } = require("ethers");
 const keccak256 = require("keccak256");
 const fs = require('fs');
 
 function main() {
+  // IMPORTANT 
+  // Increment the index [0, ...] for each tokens
   const data = [
     {
       "symbol": "SDT",
       "address": "0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F",
       "image": "https://img.api.cryptorank.io/coins/stake%20dao1611223377376.png",
       "users": [
-        { index: 0, address: "0xb7BFcDC3a2AA2aF3Fe653C9E8a19830977E1993C", amount: 1 },
+        { index: 0, address: "0xb7BFcDC3a2AA2aF3Fe653C9E8a19830977E1993C", amount: BigNumber.from(1).mul(BigNumber.from(10).pow(18)) },
+        { index: 1, address: "0xc7BFcDC3a2AA2aF3Fe653C9E8a19830977E1993C", amount: BigNumber.from(1).mul(BigNumber.from(10).pow(18)) },
       ]
     }
   ];
@@ -35,7 +38,6 @@ function main() {
         index: user.index,
         amount: user.amount,
         proof: merkleTree.getHexProof(elements[i]),
-        root: merkleTree.getHexRoot(),
       };
     }
 
@@ -43,7 +45,8 @@ function main() {
       "symbol": elem.symbol,
       "address": elem.address,
       "image": elem.image,
-      "merkle": res
+      "merkle": res,
+      root: merkleTree.getHexRoot(),
     });
   }
 
